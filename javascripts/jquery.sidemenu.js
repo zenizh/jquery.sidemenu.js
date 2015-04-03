a1d99e4801c6e3965a211a054606459add6497d8
kami <hiroki.zenigami@gmail.com>
2015-04-03 12:58:03 +0900

    Add private methods

--- javascripts/jquery.sidemenu.js
+++ javascripts/jquery.sidemenu.js
@@ -1,17 +1,33 @@
 ;(function($) {
-  $.sidemenu = function(options) {
-    $.sidemenu.options = $.extend($.sidemenu.options, options);
-  };
-
-  $.sidemenu.options = {
+  var settings = {
     direction: 'left',
     speed:     1000
   };
 
-  $.fn.sidemenu = function(action) {
-    action = action || 'open';
+  var methods = {
+    open: function(object) {
+      $(object.hash).show();
+    },
+    close: function(object) {
+      $(object.hash).hide();
+    },
+    toggle: function(object) {
+      if ($(object.hash).is(':visible')) {
+        this.close(object);
+      } else {
+        this.open(object);
+      }
+    }
+  };
 
-    $(this.hash).show();
+  $.sidemenu = function(options) {
+    settings = $.extend(settings, options);
+  };
+
+  $.fn.sidemenu = function(name) {
+    name = name || 'open';
+
+    methods[name](this);
   };
 
   $(document).ready(function() {
