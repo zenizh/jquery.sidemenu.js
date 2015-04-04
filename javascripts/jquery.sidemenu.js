a2350990ea6a20f6542359b4825ed99dc3879183
kami <hiroki.zenigami@gmail.com>
2015-04-04 10:29:07 +0900

    Add prototype

--- javascripts/jquery.sidemenu.js
+++ javascripts/jquery.sidemenu.js
@@ -1,21 +1,39 @@
 ;(function($) {
   var settings = {
     direction: 'left',
-    speed:     1000
+    speed:     400
   };
 
+  var $html,
+      $body,
+      $menu;
+
+  function hash(key, value) {
+    var hash = {};
+    hash[key] = value;
+    return hash;
+  }
+
   var methods = {
-    open: function(object) {
-      $(object.hash).show();
+    open: function() {
+      $body.css({
+        overflowX: 'hidden',
+        position:  'absolute',
+        width:     $html.width()
+      });
+
+      $body.animate(hash(settings.direction, '240px'), settings.speed);
+      $menu.animate(hash(settings.direction, '0px'),   settings.speed);
     },
-    close: function(object) {
-      $(object.hash).hide();
+    close: function() {
+      $body.animate(hash(settings.direction, '0px'),    settings.speed, function() { $(this).removeAttr('style') });
+      $menu.animate(hash(settings.direction, '-240px'), settings.speed);
     },
-    toggle: function(object) {
-      if ($(object.hash).is(':visible')) {
-        this.close(object);
+    toggle: function() {
+      if ($menu.css(settings.direction) == '0px') {
+        this.close();
       } else {
-        this.open(object);
+        this.open();
       }
     }
   };
@@ -25,16 +43,20 @@
   };
 
   $.fn.sidemenu = function(name) {
-    name = name || 'open';
+    name = name || 'toggle';
 
-    methods[name](this);
+    $html = $('html');
+    $body = $('body');
+    $menu = $(this);
+
+    methods[name]();
   };
 
   $(document).ready(function() {
-    $('[data-role="sidemenu"]').hide();
+    $('[data-role="sidemenu"]').css(settings.direction, '-240px');
 
     $('[data-role="sidemenu-toggle"]').on('click', function() {
-      $(this).sidemenu.call(this, 'toggle');
+      $(this).sidemenu.call($(this.hash));
     });
   });
 })(jQuery);
